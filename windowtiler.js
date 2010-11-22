d1fc6b52f7d2aeb0169c20285bdfc56c19b1f2fc
Manu Cornet <manu.cornet@gmail.com>
2010-11-22 15:55:37 +0100

    Added compareAreas functon, and a test

--- windowtiler.js
+++ windowtiler.js
@@ -26,6 +26,22 @@ WindowTiler.prototype.start = function(tab) {
       bind(this.onReceivedWindowsData, this));
 };
 
+WindowTiler.prototype.compareAreas = function(a, b) {
+  if (a.width != b.width) {
+    return a.width - b.width;
+  }
+  if (a.height != b.height) {
+    return a.height - b.height;
+  }
+  if (a.left != b.left) {
+    return a.left - b.left;
+  }
+  if (a.top != b.top) {
+    return a.top - b.top;
+  }
+  return 0;
+};
+
 WindowTiler.prototype.onReceivedWindowsData = function(windows) {
   this.allWindows = windows;
   this.measurementWindow = this.allWindows[0];
