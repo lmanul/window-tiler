b7893a6809d9e48c2d66f2d01e87e9c57ecfb6fa
James F. Carroll <jcarroll@byu.net>
2020-11-22 03:08:51 -0600

    Also listen for tab "detached" and "removed"
    See additional tab events: https://developer.chrome.com/extensions/tabs
    
    Omitting onAtached event. Goal is to replicate ChromeOS behavior.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var windowTiler = new WindowTiler();
 
-// Ideally, I am looking for a "new window" event, but a "new tab" event will work
+// Ideally, I am looking for a "new window" event, but a "tab" creation and removal events will work
 chrome.tabs.onCreated.addListener(windowTiler.start.bind(windowTiler));
-
+chrome.tabs.onDetached.addListener(windowTiler.start.bind(windowTiler));
+chrome.tabs.onRemoved.addListener(windowTiler.start.bind(windowTiler));
