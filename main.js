815f1aedb10c059293a597f2ca5e842f3e297059
James F. Carroll <jcarroll@byu.net>
2020-11-22 03:16:19 -0600

    Automatically tile when opening or closing a tab
    _Removed_ Detached trigger.

--- main.js
+++ main.js
@@ -2,5 +2,4 @@ var windowTiler = new WindowTiler();
 
 // Ideally, I am looking for a "new window" event, but a "tab" creation and removal events will work
 chrome.tabs.onCreated.addListener(windowTiler.start.bind(windowTiler));
-chrome.tabs.onDetached.addListener(windowTiler.start.bind(windowTiler));
 chrome.tabs.onRemoved.addListener(windowTiler.start.bind(windowTiler));
